c4e582ce206caa6449286805b0168077b748fc9d
Ben <ben.a.burgos@gmail.com>
2022-07-13 22:07:31 -0700

    adds about route

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("<h1>Woof! ฅ^•ﻌ•^ฅ</h1>")
+
+
+def about(request):
+    return HttpResponse("<h1>You're at the about page!<h1>")
