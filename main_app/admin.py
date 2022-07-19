e497d4d8fc3fc7db3cf2e8a4a31ce9b9cae856dd
Ben <ben.a.burgos@gmail.com>
2022-07-18 21:34:59 -0700

    registered Photo

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Dog, FavoriteTreat, Meal
+from .models import Dog, FavoriteTreat, Meal, Photo
 
 admin.site.register(Dog)
 admin.site.register(FavoriteTreat)
 admin.site.register(Meal)
+admin.site.register(Photo)
