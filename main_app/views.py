4842ce6dd45a3e54b460e0047b781833d53c992e
Ben <ben.a.burgos@gmail.com>
2022-07-19 13:20:01 -0700

    implements auth

--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,8 @@ from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Dog, FavoriteTreat, Photo
 from .forms import MealForm
 import boto3
@@ -22,11 +24,12 @@ def about(request):
     return render(request, 'about.html')
 
 
+@login_required
 def dogs_index(request):
-    dogs = Dog.objects.all()
+    dogs = Dog.objects.filter(user=request.user)
     return render(request, 'dogs/index.html', {'dogs': dogs})
 
-
+@login_required
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
     treats_dog_doesnt_like = FavoriteTreat.objects.exclude(
@@ -34,12 +37,12 @@ def dogs_detail(request, dog_id):
     meal_form = MealForm()
     return render(request, 'dogs/detail.html', {'dog': dog, 'meal_form': meal_form, 'treats': treats_dog_doesnt_like})
 
-
+@login_required
 def assoc_treat(request, dog_id, treat_id):
     Dog.objects.get(id=dog_id).treats.add(treat_id)
     return redirect('detail', dog_id=dog_id)
 
-
+@login_required
 def add_meal(request, dog_id):
     form = MealForm(request.POST)
     if form.is_valid():
@@ -48,7 +51,7 @@ def add_meal(request, dog_id):
         new_feeding.save()
     return redirect('detail', dog_id=dog_id)
 
-
+@login_required
 def add_photo(request, dog_id):
     photo_file = request.FILES.get('photo-file', None)
     if photo_file:
@@ -80,7 +83,7 @@ def signup(request):
     return render(request, 'registration/signup.html', context)
 
 
-class DogCreate(CreateView):
+class DogCreate(LoginRequiredMixin, CreateView):
     model = Dog
     fields = ['name', 'breed', 'description', 'age']
     success_url = '/dogs/'
@@ -90,34 +93,34 @@ class DogCreate(CreateView):
         return super().form_valid(form)
 
 
-class DogUpdate(UpdateView):
+class DogUpdate(LoginRequiredMixin, UpdateView):
     model = Dog
     fields = ['breed', 'description', 'age']
 
 
-class DogDelete(DeleteView):
+class DogDelete(LoginRequiredMixin, DeleteView):
     model = Dog
     success_url = '/dogs/'
 
 
-class FavoriteTreatList(ListView):
+class FavoriteTreatList(LoginRequiredMixin, ListView):
     model = FavoriteTreat
 
 
-class FavoriteTreatDetail(DetailView):
+class FavoriteTreatDetail(LoginRequiredMixin, DetailView):
     model = FavoriteTreat
 
 
-class FavoriteTreatCreate(CreateView):
+class FavoriteTreatCreate(LoginRequiredMixin, CreateView):
     model = FavoriteTreat
     fields = '__all__'
 
 
-class FavoriteTreatUpdate(UpdateView):
+class FavoriteTreatUpdate(LoginRequiredMixin, UpdateView):
     model = FavoriteTreat
     fields = ['name', 'flavor']
 
 
-class FavoriteTreatDelete(DeleteView):
+class FavoriteTreatDelete(LoginRequiredMixin, DeleteView):
     model = FavoriteTreat
     success_url = '/treats/'
