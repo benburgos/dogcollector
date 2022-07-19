ee3e26371a218250f257d51de56ade9bc83229b8
Ben <ben.a.burgos@gmail.com>
2022-07-19 13:14:44 -0700

    implements signup

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,10 @@
+from distutils.log import error
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from .models import Dog, FavoriteTreat, Photo
 from .forms import MealForm
 import boto3
@@ -50,7 +53,8 @@ def add_photo(request, dog_id):
     photo_file = request.FILES.get('photo-file', None)
     if photo_file:
         s3 = boto3.client('s3')
-        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        key = uuid.uuid4().hex[:6] + \
+            photo_file.name[photo_file.name.rfind('.'):]
         try:
             s3.upload_fileobj(photo_file, BUCKET, key)
             url = f"{S3_BASE_URL}{BUCKET}/{key}"
@@ -61,6 +65,21 @@ def add_photo(request, dog_id):
     return redirect('detail', dog_id=dog_id)
 
 
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up - try again!'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
+
+
 class DogCreate(CreateView):
     model = Dog
     fields = ['name', 'breed', 'description', 'age']
@@ -69,6 +88,7 @@ class DogCreate(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
 
 class DogUpdate(UpdateView):
     model = Dog
