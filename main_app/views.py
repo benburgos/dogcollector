bc43c296bdf67b9de1ec4814b1e32d7bb31bffb0
Ben <ben.a.burgos@gmail.com>
2022-07-18 17:45:45 -0700

    adds Create form

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView
 from .models import Dog
 
 
@@ -15,6 +16,13 @@ def dogs_index(request):
     dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', {'dogs': dogs})
 
+
 def dogs_detail(request, dog_id):
     dog = Dog.objects.get(id=dog_id)
-    return render(request, 'dogs/detail.html', {'dog':dog})
+    return render(request, 'dogs/detail.html', {'dog': dog})
+
+
+class DogCreate(CreateView):
+    model = Dog
+    fields = '__all__'
+    success_url = '/dogs/'
